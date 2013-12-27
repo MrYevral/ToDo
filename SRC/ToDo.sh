5bb0614d72fe1de288a29f6ba61d6ee203a100ac
MrYevral <hac22@aber.ac.uk>
2013-12-27 00:29:28 +0000

    added more informative echos, and the start of the .vim file

--- SRC/ToDo.sh
+++ SRC/ToDo.sh
@@ -1,4 +1,5 @@
-echo 'Hello, World'
+echo 'Welcome to ToDo V 0.0.1'
+echo 'Please enter a date'
 # NB no '$' in front of time variable as we are not de-referencing - doah!
 read time
 date +%Y-%m-%d_%R --date "$time"
--- SRC/vimsyntaxexample.vim
+++ SRC/vimsyntaxexample.vim
@@ -1,1 +1,8 @@
-"This is a comment
+" Vim syntax file
+" Language: ToDo
+" Maintainer: MrYevral
+" Maintainer: llamawright
+" Latest Revision: 27/12/2013
+if exsists("b:current_syntax")
+	finish
+endif
